b1043e927018999a682d6a6621052dd6cb7196f8
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-06 18:33:11 +0200

    create CucumberStep

---
+++ src/test/java/hu/webler/weblercucumberintegration/cucumberglue/CucumberStep.java
@@ -0,0 +1,29 @@
+package hu.webler.weblercucumberintegration.cucumberglue;
+
+import hu.webler.weblercucumberintegration.model.UserRegistrationRequest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@CucumberContextConfiguration
+@Getter
+@Setter
+public class CucumberStep {
+
+    @LocalServerPort
+    private int port;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private ResponseEntity<String> lastResponse;
+
+    public void sendRegisterRequest(String username, String password) {
+        String url = "http://loclahos:" + port + "/api/register";
+        UserRegistrationRequest request = new UserRegistrationRequest(username, password);
+        lastResponse = restTemplate.postForEntity(url, request, String.class);
+    }
+}
