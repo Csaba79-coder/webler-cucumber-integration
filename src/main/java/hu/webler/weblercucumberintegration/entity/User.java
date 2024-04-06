f7f4ef1e5d48f33e4d54f39732673faa2019edbe
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-06 17:24:37 +0200

    create User entity

---
+++ src/main/java/hu/webler/weblercucumberintegration/entity/User.java
@@ -0,0 +1,25 @@
+package hu.webler.weblercucumberintegration.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cucumber_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
