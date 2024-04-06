e970a962516e44f566409c3ad3f5217bb1b5f8fb
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-06 17:50:18 +0200

    create UserService

---
+++ src/main/java/hu/webler/weblercucumberintegration/service/UserService.java
@@ -0,0 +1,32 @@
+package hu.webler.weblercucumberintegration.service;
+
+import hu.webler.weblercucumberintegration.controller.exception.UsernameAlreadyExistsException;
+import hu.webler.weblercucumberintegration.entity.User;
+import hu.webler.weblercucumberintegration.model.UserRegistrationRequest;
+import hu.webler.weblercucumberintegration.model.UserRegistrationResponse;
+import hu.webler.weblercucumberintegration.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserRegistrationResponse saveUser(UserRegistrationRequest request) {
+        String username = request.getUsername();
+        Optional<User> existingUser = userRepository.findUserByUsername(username);
+
+        if (existingUser.isPresent()) {
+            String message = String.format("User with username: %s already exists!" , username);
+            log.info(message);
+            throw new UsernameAlreadyExistsException(message);
+        }
+        return null;
+    }
+}
