5c935c47fe37095a7e0a1285a3bb257362c0dff1
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-06 17:49:39 +0200

    create UsernameAlreadyExistsException

---
+++ src/main/java/hu/webler/weblercucumberintegration/controller/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package hu.webler.weblercucumberintegration.controller.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
