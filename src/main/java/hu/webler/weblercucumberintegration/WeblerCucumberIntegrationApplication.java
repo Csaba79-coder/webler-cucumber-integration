2a68c512a628f36e061ba507e780dedfd5722257
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-06 18:26:19 +0200

    add useful links / websites

--- src/main/java/hu/webler/weblercucumberintegration/WeblerCucumberIntegrationApplication.java
+++ src/main/java/hu/webler/weblercucumberintegration/WeblerCucumberIntegrationApplication.java
@@ -3,6 +3,11 @@ package hu.webler.weblercucumberintegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://www.youtube.com/watch?v=fl9pv_lZVmA
+// https://github.com/ekim197711/springboot-cucumber/blob/main/pom.xml
+// https://www.youtube.com/watch?v=TBxb5mx2MvI
+// https://github.com/ekim197711
+
 @SpringBootApplication
 public class WeblerCucumberIntegrationApplication {
 
