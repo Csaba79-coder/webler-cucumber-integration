fcdf4370d18a0f559e6ff9932625bb4733b43b42
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-06 17:17:10 +0200

    Initial commit

---
+++ src/main/java/hu/webler/weblercucumberintegration/WeblerCucumberIntegrationApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblercucumberintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerCucumberIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerCucumberIntegrationApplication.class, args);
+	}
+
+}
